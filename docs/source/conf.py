af6622924e31ec5223ff9d49b0774d05b4c4f0a2
dawnwages <dawn.wages@gmail.com>
2024-06-06 00:03:40 -0400

    add gh pages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = '2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
